20da128d730a444572969332afffde11251c3737
2206245r <2206245r@student.gla.ac.uk>
2019-02-22 23:39:43 +0000

    Add gravitation!

--- Assets/Scripts/gravitation.cs
+++ Assets/Scripts/gravitation.cs
@@ -1,38 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour {
+public class gravitation : MonoBehaviour {
     GameObject player;
     GameObject[] entities;
     Vector2 velocity = new Vector2(0,0);
+    public float G = 100;
 
     // Start is called before the first frame update
     void Start() {
         player = gameObject;
-        entities = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        entities = FindObjectsOfType<GameObject>();
+        Debug.Log("entity 1 is: " + entities[0]);
+        Debug.Log("entity 2 is: " + entities[1]);
+        Debug.Log("entity 3 is: " + entities[2]);
+        Debug.Log("entity 4 is: " + entities[3]);
+        Debug.Log("entity 5 is: " + entities[4]);
+        Debug.Log("entity 6 is: " + entities[5]);
+        Debug.Log("entity 7 is: " + entities[6]);
+
     }
       
     // Update is called once per frame
     void Update() {
+        Debug.Log("hello");
         Vector2[] components = new Vector2[entities.Length];
         for (int i = 0; i < components.Length; i++) {
-            if (entities[i] == player) {
+            Debug.Log("entity "+i+ " is" + entities[i]);
+            if (entities[i] == player || entities[i].GetComponent<info>() == null) {
+                Debug.Log("player? is" + entities[i]);
                     continue;
             }
             Vector2 entity_vector = entities[i].GetComponent<Transform>().position;
             Vector2 player_vector = player.GetComponent<Transform>().position;
             Vector2 separation_vector = entity_vector - player_vector;
-            float player_mass = player.GetComponent<Rigidbody2D>().mass;
-            float other_mass = entities[i].GetComponent<Rigidbody2D>().mass;
+            float player_mass = player.GetComponent<info>().mass;
+            float other_mass = entities[i].GetComponent<info>().mass;
             components[i] = Acceleration(player_mass, other_mass, separation_vector);
-            velocity = FindResultant(components);
+            Debug.Log("component is: "+ components[i]);
         }
-        
+        player.GetComponent<Rigidbody2D>().velocity = FindResultant(components);
     }
 
     Vector2 Acceleration(float m, float M, Vector2 r_vector) {
-        float G = 1;
         float r = r_vector.magnitude;
         Vector2 direction = r_vector / r;
         return direction * (G * m * M) / (r * r);
