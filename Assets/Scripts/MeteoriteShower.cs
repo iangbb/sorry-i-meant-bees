b30206df436221f4107dd36171ee88e997852654
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-02-23 17:14:38 +0000

    Meteor shower alert text

--- Assets/Scripts/MeteoriteShower.cs
+++ Assets/Scripts/MeteoriteShower.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeteoriteShower : MeteoriteSpawner
 {
-    
+
+    public Text meteorShowerAlertText;
     
     // Start is called before the first frame update
     protected new void Start()
@@ -27,7 +29,15 @@ public class MeteoriteShower : MeteoriteSpawner
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(30.0f, 60.0f));
+            StartCoroutine(ShowMeteoriteShowerAlert());
             StartCoroutine(SpawnMeteorite());
         }
+    }
+
+    private IEnumerator ShowMeteoriteShowerAlert()
+    {
+        meteorShowerAlertText.enabled = true;
+        yield return new WaitForSeconds(2);
+        meteorShowerAlertText.enabled = false;
     }
 }
