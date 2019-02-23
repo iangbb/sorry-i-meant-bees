2d5c193a6c71b1224ace9ffea2e67cd5e8571b92
Jamie <jhfmoore@gmail.com>
2019-02-23 01:43:27 +0000

    Nice movement, g'night

--- Assets/Scripts/gravitation.cs
+++ Assets/Scripts/gravitation.cs
@@ -10,24 +10,13 @@ public class gravitation : MonoBehaviour {
     void Start() {
         player = gameObject;
         entities = FindObjectsOfType<GameObject>();
-        Debug.Log("entity 1 is: " + entities[0]);
-        Debug.Log("entity 2 is: " + entities[1]);
-        Debug.Log("entity 3 is: " + entities[2]);
-        Debug.Log("entity 4 is: " + entities[3]);
-        Debug.Log("entity 5 is: " + entities[4]);
-        Debug.Log("entity 6 is: " + entities[5]);
-        Debug.Log("entity 7 is: " + entities[6]);
-
     }
       
     // Update is called once per frame
     void Update() {
-        Debug.Log("hello");
         Vector2[] components = new Vector2[entities.Length];
         for (int i = 0; i < components.Length; i++) {
-            Debug.Log("entity "+i+ " is" + entities[i]);
             if (entities[i] == player || entities[i].GetComponent<info>() == null) {
-                Debug.Log("player? is" + entities[i]);
                     continue;
             }
             Vector2 entity_vector = entities[i].GetComponent<Transform>().position;
@@ -36,15 +25,14 @@ public class gravitation : MonoBehaviour {
             float player_mass = player.GetComponent<info>().mass;
             float other_mass = entities[i].GetComponent<info>().mass;
             components[i] = Acceleration(player_mass, other_mass, separation_vector);
-            Debug.Log("component is: "+ components[i]);
         }
-        player.GetComponent<Rigidbody2D>().velocity = FindResultant(components);
+        player.GetComponent<Rigidbody2D>().velocity += FindResultant(components);
     }
 
-    Vector2 Acceleration(float m, float M, Vector2 r_vector) {
+    Vector2 Acceleration(float m1, float m2, Vector2 r_vector) {
         float r = r_vector.magnitude;
         Vector2 direction = r_vector / r;
-        return direction * (G * m * M) / (r * r);
+        return direction * (G * m1 * m2) / (r * r);
     }
 
     Vector2 FindResultant(Vector2[] components) {
