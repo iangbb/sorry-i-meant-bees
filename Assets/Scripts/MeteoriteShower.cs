c0c0d83ff6ce97f915a7dc5a7a13050b4530a07c
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-02-23 16:48:45 +0000

    Refactor meteors to use coroutines

---
+++ Assets/Scripts/MeteoriteShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteShower : MeteoriteSpawner
+{
+    
+    
+    // Start is called before the first frame update
+    protected new void Start()
+    {
+        //activationTime = Random.Range(30.0f, 60.0f);
+        minSpawnInterval = 0.2f;
+        maxSpawnInterval = 1.0f;
+        totalMeteorsToSpawn = 20;
+        StartCoroutine(DelayedActivation(10.0f));
+    }
+
+    private IEnumerator DelayedActivation(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        base.Start();
+    }
+}
--- Assets/Scripts/MeteoriteSpawner.cs
+++ Assets/Scripts/MeteoriteSpawner.cs
@@ -14,25 +14,32 @@ public class MeteoriteSpawner : MonoBehaviour
     private float spawnTimer;
     private float width;
     private float height;
+    protected int totalMeteorsToSpawn = 0;
 
     private List<GameObject> players;
 
-    void Start()
+    protected void Start()
     {
-        spawnTimer = Random.Range(minSpawnInterval, maxSpawnInterval);
         GameObject cont = GameObject.FindGameObjectWithTag("GameController");
         width = cont.GetComponent<boundary_script>().width;
         height = cont.GetComponent<boundary_script>().height;
         players = new List<GameObject>();
         foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")) players.Add(player);
+        StartCoroutine(SpawnMeteorite());
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0)
+        
+    }
+
+    protected IEnumerator SpawnMeteorite()
+    {
+        int meteorsSpawned = 0;
+        while (totalMeteorsToSpawn == 0 || meteorsSpawned++ < totalMeteorsToSpawn)
         {
+            yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
             int edge = Random.Range(0, 4);
             float posX, posY;
 
@@ -46,14 +53,12 @@ public class MeteoriteSpawner : MonoBehaviour
                 posY = Random.Range(-1 * (height / 2), height / 2);
                 posX = (width / 2) * (edge - 2);
             }
-            
+
             GameObject newMet = Instantiate(meteorite, new Vector2(posX, posY), new Quaternion());
             GameObject target = players[Random.Range(0, players.Count - 1)];
             Vector2 direction = target.transform.position - newMet.transform.position;
             float meteoriteSpeed = Random.Range(minMeteoriteSpeed, maxMeteoriteSpeed);
             newMet.GetComponent<Rigidbody2D>().AddForce(meteoriteSpeed * direction / direction.magnitude);
-
-            spawnTimer = Random.Range(minSpawnInterval, maxSpawnInterval);
         }
     }
 }
