7af098d6141b4d3a9c8e19963ecd4b93897f4f40
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-02-23 17:01:10 +0000

    Loop meteor shower

--- Assets/Scripts/MeteoriteShower.cs
+++ Assets/Scripts/MeteoriteShower.cs
@@ -9,16 +9,25 @@ public class MeteoriteShower : MeteoriteSpawner
     // Start is called before the first frame update
     protected new void Start()
     {
-        //activationTime = Random.Range(30.0f, 60.0f);
         minSpawnInterval = 0.2f;
         maxSpawnInterval = 1.0f;
         totalMeteorsToSpawn = 20;
-        StartCoroutine(DelayedActivation(10.0f));
+        StartCoroutine(DelayedActivation(Random.Range(30.0f, 60.0f)));
     }
 
     private IEnumerator DelayedActivation(float delay)
     {
         yield return new WaitForSeconds(delay);
         base.Start();
+        StartCoroutine(LoopMeteorShowerAfterRandomTime());
+    }
+
+    private IEnumerator LoopMeteorShowerAfterRandomTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(30.0f, 60.0f));
+            StartCoroutine(SpawnMeteorite());
+        }
     }
 }
