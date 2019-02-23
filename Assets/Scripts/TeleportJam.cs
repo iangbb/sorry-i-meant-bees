b99fd645cd034605fc8192271ab6d0ab14b28818
Sohva <33163737+Sohva@users.noreply.github.com>
2019-02-23 17:39:10 +0000

    Teleport Jam now works!

---
+++ Assets/Scripts/TeleportJam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportJam : JamScript
+{
+    public override void jamEffect(GameObject player)
+    {
+        player.GetComponent<portal_activate>().setPortalEnabled(true);
+        print("Teleport your Enemies with this JAM!!!!!!!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
