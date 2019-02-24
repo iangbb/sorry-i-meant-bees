5404be66fa7fac4754754283fbb9af2cdde40dfc
Sohva <33163737+Sohva@users.noreply.github.com>
2019-02-24 12:27:47 +0000

    Working on the controller

--- Assets/Scripts/MainSceneController.cs
+++ Assets/Scripts/MainSceneController.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string endScene;
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public Text player1Controls;
+    public Text player2Controls;
+
+    public float showControlTime = 10;
+
     void Start()
     {
         
@@ -14,5 +25,10 @@ public class MainSceneController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private IEnumerator showControls(GameObject player, Text text)
+    {
+        yield return new WaitForSeconds(showControlTime);
     }
 }
