9c5ae5b189268174f352c823ec70927bc5826ed5
Sohva <33163737+Sohva@users.noreply.github.com>
2019-02-24 11:35:30 +0000

    Changes to the scene¨

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
     public string gameSceneName;
 
+    public Text introText;
+
+    public Text title;
+
+    public bool ready;
 
     void Start()
     {
+        ready = false;
+        introText.text = "";
+        title.text = "";
+        showIntro();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (ready && Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
         }
+    }
+
+    private void showIntro()
+    {
+        introText.text = "A long time ago, in a galaxy far far away...";
     }
 }
