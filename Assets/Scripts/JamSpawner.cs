b7a2fdd62dda3d18247edecc5134af5bdc07cdc5
2206245r <2206245r@student.gla.ac.uk>
2019-02-24 11:03:03 +0000

    rm prints

--- Assets/Scripts/JamSpawner.cs
+++ Assets/Scripts/JamSpawner.cs
@@ -41,7 +41,6 @@ public class JamSpawner : MonoBehaviour
             {
                 GameObject jam = jams[Random.Range(0, 2)];
                 GameObject newJam = Instantiate(jam, new Vector2(posX, posY), new Quaternion());
-                print(newJam);
             }
             spawnTimer = Random.Range(minSpawnInterval, maxSpawnInterval);
         }
