a72f5be41b4010fae463dd4a0b7dc8f2c35c0289
Sohva <33163737+Sohva@users.noreply.github.com>
2019-02-24 10:04:22 +0000

    Trying to load the screen

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public string gameSceneName;
+
+    private AssetBundle myLoadedAssetBundle;
+
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+        }
+    }
+}
