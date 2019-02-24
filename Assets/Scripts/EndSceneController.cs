de00a376acbb507bf39f31de78e2c201910b5d3d
Sohva <33163737+Sohva@users.noreply.github.com>
2019-02-24 14:44:51 +0000

    End game fixed

--- Assets/Scripts/EndSceneController.cs
+++ Assets/Scripts/EndSceneController.cs
@@ -6,11 +6,16 @@ using UnityEngine.SceneManagement;
 public class EndSceneController : MonoBehaviour
 {
     public string introScene;
+    public GameObject endImage;
+    public GameObject showStuff;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        endImage.SetActive(false);
+        showStuff.SetActive(false);
+        StartCoroutine(showEndCredits());
+     
     }
 
     // Update is called once per frame
@@ -24,5 +29,21 @@ public class EndSceneController : MonoBehaviour
             }
             SceneManager.LoadScene(introScene, LoadSceneMode.Single);
         }
+    }
+
+    private IEnumerator showEndCredits()
+    {
+        print("cool");
+        yield return new WaitForSeconds(5);
+        endImage.SetActive(true);
+        showStuff.SetActive(true);
+        print("cool");
+        GameObject.Destroy(GameObject.Find("Canvas"));
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            GameObject.Destroy(player);
+        }
+        
+
     }
 }
