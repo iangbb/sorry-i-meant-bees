1434a6f002122828b0630f01077603c3ebd78713
Jamie <jhfmoore@gmail.com>
2019-03-09 17:39:05 +0000

    Stable(ish)

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -17,9 +16,19 @@ public class SceneController : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
 
+    public GameObject player1Image;
+    public GameObject player2Image;
+
     public GameObject titleImage;
 
     public const float waitTime = 3f;
+
+    private bool showControlInfo;
+    public float showControlTime = 10;
+    public Text player1Controls;
+    public Text player2Controls;
+
+    public bool show;
 
     void Start()
     {
@@ -28,8 +37,11 @@ public class SceneController : MonoBehaviour
         title.text = "";
         player2.SetActive(false);
         player1.SetActive(false);
+        player1Image.SetActive(false);
+        player2Image.SetActive(false);
         titleImage.SetActive(false);
         StartCoroutine(showIntro());
+        showControlInfo = false;
     }
 
     // Update is called once per frame
@@ -39,22 +51,34 @@ public class SceneController : MonoBehaviour
         {
             SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) toggleControls();
+
+        if (!show)
+        {
+            player2.SetActive(false);
+            player2Image.SetActive(false);
+        }
     }
 
     private IEnumerator showIntro()
     {
+        
         yield return new WaitForSeconds(2);
         introText.text = "A long time ago,\nin a galaxy far far away...";
         ready = true;
         yield return new WaitForSeconds(waitTime);
         GetComponent<IntroMusic>().setReady(true);
         introText.text = "...there were these twins...";
-        player2.SetActive(true);
+        player1.SetActive(true);
+        player1Image.SetActive(true);
         yield return new WaitForSeconds(waitTime);
         introText.text = "...which one might describe as ...";
-        player1.SetActive(true);
         yield return new WaitForSeconds(waitTime);
         introText.text = "...quite competitive.";
+        show = true;
+        player2.SetActive(true);
+        player2Image.SetActive(true);
         yield return new WaitForSeconds(waitTime);
         introText.text = "After competing over fame,\nmoney and power...";
         yield return new WaitForSeconds(waitTime);
@@ -94,5 +118,43 @@ public class SceneController : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         introText.transform.position -= new Vector3(0, 20, 0);
         introText.text = "Press space to start.";
+    }
+    
+    private void toggleControls()
+    {
+        if (showControlInfo)
+        {
+            player1Controls.text = "";
+            player2Controls.text = "";
+            showControlInfo = false;
+        }
+        else
+        {
+            player1Controls.text = getControlText(player1);
+            player2Controls.text = getControlText(player2);
+            showControlInfo = true;
+        }
+    }
+    
+    private string getControlText(GameObject player)
+    {
+        string controlText = "";
+        playerEngine eng = player.GetComponent<playerEngine>();
+        portal_activate por = player.GetComponent<portal_activate>();
+        player_special_powers spec = player.GetComponent<player_special_powers>();
+        hookshot_script hook = player.GetComponent<hookshot_script>();
+        controlText += "Up: " + eng.up + "\n";
+        controlText += "Left: " + eng.left + "\n";
+        controlText += "Right: " + eng.right + "\n";
+        controlText += "Down: " + eng.down + "\n";
+        controlText += "Jams\n";
+        controlText += "Portal: " + por.portalKey + "\n";
+        controlText += "- Weight: " + spec.levitateKey + "\n";
+        controlText += "+ Weight: " + spec.antiLeviKey + "\n";
+        controlText += "Push: " + spec.pushKey + "\n";
+        controlText += "Pull: " + spec.pullKey + "\n";
+        controlText += "HookShot Left: " + hook.hookshotLeftKey + "\n";
+        controlText += "HookShot Right: " + hook.hookshotRightKey + "\n";
+        return controlText;
     }
 }
